79c96d067ad42fd2a653f90624424cda704d98ab
Thomas Vinadelle <35263641+Zarechi@users.noreply.github.com>
2024-11-08 11:47:28 +0100

    - Disable damages if applied to the same Affinity Character

--- Source/SkillTest_M4/Private/SkillTestCharacter.cpp
+++ Source/SkillTest_M4/Private/SkillTestCharacter.cpp
@@ -5,14 +5,14 @@
 // Sets default values
 ASkillTestCharacter::ASkillTestCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 }
 
 // Called when the game starts or when spawned
 void ASkillTestCharacter::BeginPlay()
 {
-	Super::BeginPlay();	
+	Super::BeginPlay();
 }
 
 // Called every frame
@@ -22,10 +22,13 @@ void ASkillTestCharacter::Tick(float DeltaTime)
 }
 
 float ASkillTestCharacter::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator,
-	AActor* DamageCauser)
+                                      AActor* DamageCauser)
 {
-	Health -= DamageAmount;
-	UE_LOG(LogTemp, Warning, TEXT("%s received damages : %f"), *this->GetName(), Health);
-	
+	if (Affinity != Cast<ASkillTestCharacter>(DamageCauser)->Affinity)
+	{
+		Health -= DamageAmount;
+		UE_LOG(LogTemp, Warning, TEXT("%s received damages : %f"), *this->GetName(), Health);
+	}
+
 	return Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
 }
